58945326daec9ad7874fd518a07bb8b798d95bb7
zhangxing789 <2930281671@qq.com>
2018-10-20 14:03:03 +0800

    使用easy mock

---
+++ ikcamp/pages/index/index.js
@@ -0,0 +1,69 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    hiddenLoading: false,
+    page: 1,
+    pageSize: 4,
+    days: 3,
+    totalSize: 0,
+    hasMore: true,
+    articleList: [],
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    this.requestArticle()
+  },  
+  
+  requestArticle: function () {
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5bca9250e6742c1bf8220bdf/ikcamp/list#!method=get',
+        success: (res) => {
+          console.log(res.data.data);
+          let list = this.data.articleList;
+          list = [...list, ...res.data.data];
+          console.log(list);
+          // const list = this.data.articleList;
+          list.push(res.data.data[this.data.page-1])
+          console.log(list)
+          this.setData({
+            hiddenLoading:true,
+            articleList:list
+  
+          })
+      }
+    })
+  },
+  
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+    
+  },
+
+  onReachBottom() {
+    if(this.data.hasMore) {
+      let nextPage = this.data.page + 1;
+      this.setData({
+        page: nextPage
+      });
+      this.requestArticle();
+    }
+    console.log('到页面底部了');
+  }
+})
