2dbb3c6910243dd5c58f5b422b5b9a3b2706525d
张幸 <zhangxing@zhangxingdeMacBook-Pro.local>
2018-11-21 12:41:38 +0800

    弹幕

---
+++ html5/zjl/webpack.config.js
@@ -0,0 +1,30 @@
+//node common.js
+const path = require('path');
+module.exports = {
+    // 入口
+    entry: './src/main.js',
+    output: {
+        //打包
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '.')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                exclude: '/node_modules/'
+            }
+        ]
+
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    devServer: {
+        contentBase: '.'
+    }
+    
+}
