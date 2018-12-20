6e38a28d81cd0b26692b9daf653f658f2950445b
zhangxing789 <2930281671@qq.com>
2018-12-20 21:47:10 +0800

    vue

---
+++ vue/eleme/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import header from '@/components/header/header.vue';
+const response = require('./common/data/seller.json');
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  created () {
+    // fetch() axios 
+    this.seller = Object.assign({},this.seller, response.data)
+  },
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/eleme/src/components/goods/goods.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="goods" ref="menuWrapper">
+            <ul>
+                <li v-for="(item, index) in goods" :key="index" class="menu-item">
+                    <span class="text border-1px">
+                        {{item.name}}
+                    </span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            goods: [
+                { name: '小笼包' },
+                { name: '水饺' }
+            ]
+        }
+    }
+}
+</script>
+<style lang="stylus">
+.goods
+    display flex
+    
+</style>
+
